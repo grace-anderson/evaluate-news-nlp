622a857b3156bdf2c85a74a1184e7660e4bd3923
GracieM <grace.and.lucy.nz@gmail.com>
2021-03-06 14:10:28 +1100

    Added API

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,63 @@
+const dotenv = require('dotenv');
+const express = require('express')
+const bodyParser = require("body-parser");
+const axios = require('axios')
+
+dotenv.config();
+
+const router = express.Router();
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+app.listen(8081, function () {
+  console.log('Example app listening on port 8081!')
+})
+
+app.post('/summarization', async (req, res) => {
+  const API = "http://api.meaningcloud.com/summarization-1.0?key="
+  const API_KEY = process.env.API_KEY
+  const text = req.body.text
+  const number = req.body.number
+  const url = req.body.url
+  const params = text ? `&txt=${text}&sentences=${number}` : `&url=${url}&sentences=${number}`
+  const response = await axios.post(encodeURI(API + API_KEY + params), {})
+
+  try {
+    const result = JSON.stringify(response.data.summary)
+    res.send(result)
+  }
+  catch (error) {
+    console.log('error', error)
+  }
+
+})
+
+
+
+/* const dotenv = require('dotenv');
+dotenv.config();
+
+console.log('Your API key is ${process.env.API_KEY}');
+
+// API credentials
+var textapi = new meaningCloud({
+    application_key: process.env.API_KEY
+ });
+
+ console.log('Your API key is ${process.env.API_KEY}');
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -20,4 +80,4 @@ app.listen(8081, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+}) */
